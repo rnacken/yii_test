e319af164e182985e88633e3504e61a10e8a9ae8
Ru <info@nackenproducties.nl>
2013-01-18 10:36:11 +0100

    Update protected/views/site/contact.php

--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -3,7 +3,7 @@
 /* @var $model ContactForm */
 /* @var $form CActiveForm */
 
-$this->pageTitle=Yii::app()->name . ' - Contact Us';
+$this->pageTitle=Yii::app()->name . ' -- Contact Us';
 $this->breadcrumbs=array(
 	'Contact',
 );
